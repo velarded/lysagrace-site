93f6763a2673b32db34190a7e7b7b89322d6f548
Danica <danica.cristine.velarde@gmail.com>
2024-03-05 13:47:18 -0500

    fix iframe properties

--- app/main/media/page.tsx
+++ app/main/media/page.tsx
@@ -9,16 +9,16 @@ export default function Media() {
           <div className="snap-center shrink-0">
             </div>
             <div className="snap-center shrink-0">
-              <iframe className="z-10" width="1000" height="500" src="https://www.youtube.com/embed/abrAI1iwS0g" title="The Voice Teens Philippines Blind Audition: Alyssa Datu - Hanggang" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              <iframe className="z-10" width="1000" height="500" src="https://www.youtube.com/embed/abrAI1iwS0g" title="The Voice Teens Philippines Blind Audition: Alyssa Datu - Hanggang" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
             <div className="snap-center shrink-0">
-              <iframe width="1000" height="500" src="https://www.youtube.com/embed/4CizNYohkO4" title="Forever By Martin Nievera and Alyssa Grace | MAPLE MADNESS with Martin Nievera" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              <iframe width="1000" height="500" src="https://www.youtube.com/embed/4CizNYohkO4" title="Forever By Martin Nievera and Alyssa Grace | MAPLE MADNESS with Martin Nievera" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
             <div className="snap-center shrink-0">
-            <iframe width="1000" height="500" src="https://www.youtube.com/embed/2pGwnZYKxr4" title="Please Be Careful With My Heart Cover | Alyssa Grace Duet w/ Raymond Lauchengco" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe width="1000" height="500" src="https://www.youtube.com/embed/2pGwnZYKxr4" title="Please Be Careful With My Heart Cover | Alyssa Grace Duet w/ Raymond Lauchengco" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
             <div className="snap-center shrink-0">
-              <iframe width="1000" height="500" src="https://www.youtube.com/embed/_jXiRGNLwL8" title="Ngayon At Kailanman By Alyssa Grace" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              <iframe width="1000" height="500" src="https://www.youtube.com/embed/_jXiRGNLwL8" title="Ngayon At Kailanman By Alyssa Grace" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
           </div>
         </div>
